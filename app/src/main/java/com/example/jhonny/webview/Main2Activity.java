90754a4aa0f2409d9d4e43ef650be43cad34460a
jaospina28 <jhonny.ospina@uniremingtonmanizales.edu.co>
2017-05-09 20:13:34 -0500

    Se creo la primera actividad

---
+++ app/src/main/java/com/example/jhonny/webview/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.example.jhonny.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        webView = (WebView)findViewById(R.id.web);
+        webView.loadUrl("http//:www.uniremingtonmanizales.edu.co");
+    }
+}
