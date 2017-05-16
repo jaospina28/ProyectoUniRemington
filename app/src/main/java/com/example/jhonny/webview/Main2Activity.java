a501259ce089763af9fa4ed3e3c440a929218f6d
jaospina28 <jhonny.ospina@uniremingtonmanizales.edu.co>
2017-05-15 23:24:35 -0500

    Se crearon los botones, con putextra se envia pagina web a 3 activity con web view

--- app/src/main/java/com/example/jhonny/webview/Main2Activity.java
+++ app/src/main/java/com/example/jhonny/webview/Main2Activity.java
@@ -1,19 +1,58 @@
 package com.example.jhonny.webview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
 
-    WebView webView;
+    Button correo;
+    Button q10;
+    Button moodle;
+    Button recibo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        webView = (WebView)findViewById(R.id.web);
-        webView.loadUrl("http://uniremingtonmanizales.edu.co/");
+        correo = (Button) findViewById(R.id.btn_correo);
+        correo.setOnClickListener(this);
+        q10 = (Button) findViewById(R.id.btn_q10);
+        q10.setOnClickListener(this);
+        moodle = (Button) findViewById(R.id.btn_moodle);
+        moodle.setOnClickListener(this);
+        recibo = (Button) findViewById(R.id.btn_recibo);
+        recibo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_correo){
+            Intent cambioActi3 = new Intent(this, Main3Activity.class);
+            String link = "https://accounts.google.com/signin/v2/identifier?continue=http%3A%2F%2Fmail.google.com%2Fa%2Funiremington.edu.co%2F&ltmpl=default&service=mail&sacu=1&hd=uniremington.edu.co&flowName=GlifWebSignIn&flowEntry=Identifier";
+            cambioActi3.putExtra("link",link);
+            startActivity(cambioActi3);
+        }else if(v.getId() == R.id.btn_q10){
+            Intent cambioActi3 = new Intent(this, Main3Activity.class);
+            String link = "https://www.q10academico.com/login?ReturnUrl=%2F&aplentId=a2e25504-9d7b-4e4e-861a-5f015f373d8f";
+            cambioActi3.putExtra("link",link);
+            startActivity(cambioActi3);
+        }else if(v.getId() == R.id.btn_moodle){
+            Intent cambioActi3 = new Intent(this, Main3Activity.class);
+            String link = "http://virtual.uniremingtonmanizales.edu.co/moodle/login/index.php";
+            cambioActi3.putExtra("link",link);
+            startActivity(cambioActi3);
+        }else if(v.getId() == R.id.btn_recibo){
+            Intent cambioActi3 = new Intent(this, Main3Activity.class);
+            String link = "http://www.uniremingtonmanizales.edu.co/liquidacion/inicio.php";
+            cambioActi3.putExtra("link",link);
+            startActivity(cambioActi3);
+        }
     }
 }
--- app/src/main/java/com/example/jhonny/webview/Main3Activity.java
+++ app/src/main/java/com/example/jhonny/webview/Main3Activity.java
@@ -2,12 +2,29 @@ package com.example.jhonny.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
 
-public class Main3Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener{
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+
+        String link = getIntent().getStringExtra("link");
+
+        webView = (WebView)findViewById(R.id.web);
+        webView.loadUrl(link);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
